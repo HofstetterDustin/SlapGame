8974c531bf317efdc7ec322bd81700baa8458d7c
Dustin Hofstetter <Hofstetter.dustinart@gmail.com>
2018-02-02 11:35:32 -0700

    slapme3

--- game.js
+++ game.js
@@ -1,1 +1,14 @@
-I made a change
+var health = 100
+
+function slap(){
+    this.slap = slap
+
+    this.health = 100
+    this.slap = {
+        slap: 0
+    }
+
+    this.health = health
+    health--
+    health.push(this)
+}
